775597c84451ca8d26e333c5fe9c33a7cee8e77e
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2020-02-08 17:53:33 -0800

    404 errors on page load fixed

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// app.set('/views', express.static(path.join(__dirname, '/views')));
+// app.use(express.static(path.join(__dirname, '/views')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+app.listen(port);
